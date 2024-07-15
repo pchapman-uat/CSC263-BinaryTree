d3be0656822ae79c371323757cfcbe90cff5a6a1
Preston Chapman <pchapman82070@uat.edu>
2024-07-15 13:09:04 -0700

    Search Methods

--- Main.java
+++ Main.java
@@ -61,4 +61,16 @@ class BinarySearchTree{
             minValue = root.left.key;
         } return minValue;        
     }
+    public boolean search(int key){
+        return searchRec(root, key);
+    }
+    private boolean searchRec(Node root, int key){
+        if(root == null || root.key == key){
+            return root!=null;
+        }
+        if(key < root.key){
+            return searchRec(root.left, key);
+        }
+        return searchRec(root.right, key);
+    }
 }
