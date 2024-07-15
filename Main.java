f9e882a45c6bfc72aedf3239539d82e32d4c1687
Preston Chapman <pchapman82070@uat.edu>
2024-07-15 12:56:07 -0700

    Insert Key

--- Main.java
+++ Main.java
@@ -17,4 +17,18 @@ class BinarySearchTree{
     public BinarySearchTree(){
         root = null;
     }
+    public void insert(int key){
+        root = insertRec(root, key);
+    }
+    private Node insertRec(Node root, int key){
+        if(root == null){
+            root = new Node(key);
+            return root;
+        }
+        if(key<root.key){
+            root.left = insertRec(root.left, key);
+        } else if(key>root.key){
+            root.right = insertRec(root.right, key);
+        } return root;
+    }
 }
