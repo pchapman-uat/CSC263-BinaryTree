a4ff97ea320c1e975afdc6424a9387c8e7e207e6
Preston Chapman <pchapman82070@uat.edu>
2024-07-15 12:51:24 -0700

    Node Class

--- Main.java
+++ Main.java
@@ -3,3 +3,11 @@ public class Main {
         System.out.println("Hello World");
     }
 }
+class Node {
+    int key;
+    Node left, right;
+    public Node(int item){
+        key = item;
+        left = right = null;
+    }
+}
