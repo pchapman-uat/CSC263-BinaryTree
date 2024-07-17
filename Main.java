4bdba509c75c8c756f23046dfeba95f2afc89178
Preston Chapman <pchapman82070@uat.edu>
2024-07-17 12:31:19 -0700

    Add Comments for Orginization

--- Main.java
+++ Main.java
@@ -1,8 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        // Implimentation
+        
     }
 }
+
+
+// Binary Tree Class
 class Node {
     int key;
     Node left, right;
